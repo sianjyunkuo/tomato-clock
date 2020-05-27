66ebd4993c44c14b233c11527cfb38f5d4311cc8
Sian Jyun Kuo <kuosianjyun@gmail.com>
2020-05-27 20:22:00 +0800

    mission result chart

--- src/components/ChartSection.js
+++ src/components/ChartSection.js
@@ -1,8 +1,18 @@
-import React, { useMemo, memo } from "react";
+import React, { useEffect, useMemo, memo, useContext, useRef } from "react";
+import { ContextStore } from "../context";
 import classNames from "classnames";
 import cons from "../constants";
+import Chart from "chart.js";
 
 const ChartSection = ({ currentNavContentId }) => {
+  const { missions } = useContext(ContextStore);
+  const chartResultRef = useRef(null);
+
+  const totalCompleted = useMemo(() => {
+    const filteredMissions = missions.filter((mission) => mission.isCompleted);
+    return filteredMissions ? filteredMissions.length : 0;
+  }, [missions]);
+
   const renderChartSectionClass = useMemo(
     () =>
       classNames({
@@ -11,7 +21,50 @@ const ChartSection = ({ currentNavContentId }) => {
       }),
     [currentNavContentId]
   );
-  return <div className={renderChartSectionClass}>ChartSection</div>;
+  useEffect(() => {
+    new Chart(chartResultRef.current, {
+      type: "doughnut",
+      data: {
+        datasets: [
+          {
+            data: [totalCompleted, missions.length],
+            backgroundColor: ["#ffd5c0", "#ffab82"],
+          },
+        ],
+        labels: ["Completed", "Total"],
+      },
+      options: {
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        animation: {
+          animateScale: true,
+          animateRotate: true,
+        },
+        elements: {
+          borderColor: "red",
+        },
+      },
+    });
+  }, [totalCompleted, missions]);
+  return (
+    <div className={renderChartSectionClass}>
+      <div className="mission-result">
+        <div className="mission-result-item">
+          <p className="item-title">Completed</p>
+          <p className="item-context">{totalCompleted}</p>
+        </div>
+        <div className="mission-result-item">
+          <p className="item-title">Totoally</p>
+          <p className="item-context">{missions.length}</p>
+        </div>
+      </div>
+      <div className="chart-result-wrapper">
+        <canvas ref={chartResultRef} width="500" height="500"></canvas>
+      </div>
+    </div>
+  );
 };
 
 export default memo(ChartSection);
