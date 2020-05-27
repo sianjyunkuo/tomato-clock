3b18bee3dd92b3da08ee9d7aa86f1d4ebd4bb816
Sian Jyun Kuo <kuosianjyun@gmail.com>
2020-05-27 15:49:32 +0800

    Remove useless custom hook

--- src/hook/index.js
+++ src/hook/index.js
@@ -1,19 +1,3 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 
-window.TIMER = null;
-
-export const useClock = ({}) => {
-  const [isPlay, setIsPlay] = useState(false);
-  const [isBell, setIsBell] = useState(true);
-  const [time, setTime] = useState(0);
-
-  const onBtnPlay = () => {
-    time > 0 && setIsPlay((prevIsPlay) => !prevIsPlay);
-  };
-
-  const onBtnBell = () => {
-    setIsBell((prevIsBell) => !prevIsBell);
-  };
-
-  return [isPlay, isBell, progress, time, onBtnPlay, onBtnBell];
-};
+export const useClock = () => {};
