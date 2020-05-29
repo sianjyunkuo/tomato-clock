4b96cc82e2f83203a9c86d97125f43379af70f60
Sian Jyun Kuo <kuosianjyun@gmail.com>
2020-05-29 09:41:32 +0800

    Updates

---
+++ precache-manifest.b419ca79384eebf99c7b284fd6200995.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d304badcbab7df651c2bc5f8c12a1bcd",
+    "url": "/tomato-clock/index.html"
+  },
+  {
+    "revision": "7b96b57a5f2e21e3ee4d",
+    "url": "/tomato-clock/static/css/main.d9042e10.chunk.css"
+  },
+  {
+    "revision": "d108a077e521a8ba18eb",
+    "url": "/tomato-clock/static/js/2.1a668a9b.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/tomato-clock/static/js/2.1a668a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b96b57a5f2e21e3ee4d",
+    "url": "/tomato-clock/static/js/main.fbf84cc0.chunk.js"
+  },
+  {
+    "revision": "88bcdbc32beb9f738636",
+    "url": "/tomato-clock/static/js/runtime-main.04aa8bff.js"
+  },
+  {
+    "revision": "06d01a2d4b442734de40efc0dba0df47",
+    "url": "/tomato-clock/static/media/btn-arrow-down.06d01a2d.svg"
+  },
+  {
+    "revision": "27a97309d2105b4c76e1e4561f3f7744",
+    "url": "/tomato-clock/static/media/btn-bell.27a97309.svg"
+  },
+  {
+    "revision": "a9fd689d1b9b58343f40fa61fb3f8634",
+    "url": "/tomato-clock/static/media/btn-chart.a9fd689d.svg"
+  },
+  {
+    "revision": "1dcf41a365b4baec0cc0d47ee3944a59",
+    "url": "/tomato-clock/static/media/btn-config.1dcf41a3.svg"
+  },
+  {
+    "revision": "6fda9928c1496a8198ed32a9e7567b99",
+    "url": "/tomato-clock/static/media/btn-list.6fda9928.svg"
+  },
+  {
+    "revision": "1a23fcd7f939ab1c65f7f77666b7b1a2",
+    "url": "/tomato-clock/static/media/btn-next.1a23fcd7.svg"
+  },
+  {
+    "revision": "35567098c1e43e0597cde7d156d15aa3",
+    "url": "/tomato-clock/static/media/btn-pause.35567098.svg"
+  },
+  {
+    "revision": "0476a157969020d3a6763a5c64cbb76f",
+    "url": "/tomato-clock/static/media/btn-play.0476a157.svg"
+  },
+  {
+    "revision": "e9bfaab127d16f46dcc9283c46e569a7",
+    "url": "/tomato-clock/static/media/btn-unbell.e9bfaab1.svg"
+  },
+  {
+    "revision": "15731e7b7aa853adc734f810402bbf66",
+    "url": "/tomato-clock/static/media/checkbox-checked.15731e7b.svg"
+  },
+  {
+    "revision": "41642f4dde6dd5e729296ec7f31fd3dc",
+    "url": "/tomato-clock/static/media/checkbox-default.41642f4d.svg"
+  },
+  {
+    "revision": "be8c80d177574b6253a1003e3b9879a8",
+    "url": "/tomato-clock/static/media/deco1.be8c80d1.svg"
+  },
+  {
+    "revision": "4469f7831138c4bee43acba6bc4b8ab0",
+    "url": "/tomato-clock/static/media/deco2.4469f783.svg"
+  },
+  {
+    "revision": "d78d8d4bfb25db7550b5494ee839f352",
+    "url": "/tomato-clock/static/media/deco3.d78d8d4b.svg"
+  },
+  {
+    "revision": "c58bf59c133eaad65fdb3b19ec93431b",
+    "url": "/tomato-clock/static/media/deco4.c58bf59c.svg"
+  },
+  {
+    "revision": "35e519195cbc5ec1d9f93060e098204b",
+    "url": "/tomato-clock/static/media/icon-calendar.35e51919.svg"
+  },
+  {
+    "revision": "2017da30b6f5cfca2aa727f73f94b581",
+    "url": "/tomato-clock/static/media/logo.2017da30.svg"
+  },
+  {
+    "revision": "94621486e82463baf477f7b58266810c",
+    "url": "/tomato-clock/static/media/slogan.94621486.svg"
+  }
+]);
